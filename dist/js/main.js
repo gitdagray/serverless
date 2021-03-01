0ae51b31fce3247a9ee4f5bb68a8592e43593270
gitdagray <DaveOnEleven@gmail.com>
2021-03-01 13:57:07 -0600

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,26 @@
+const getRandomDadJoke = async () => {
+  const url = "https://icanhazdadjoke.com/";
+  const jokeStream = await fetch(url, {
+    headers: {
+      Accept: "application/json"
+    }
+  });
+  const jsonJoke = await jokeStream.json();
+  const joke = jsonJoke.joke;
+  return joke;
+};
+
+const displayJoke = (joke) => {
+  const h1 = document.querySelector("h1");
+  h1.textContent = joke;
+};
+
+const refreshJoke = async () => {
+  const joke = await getRandomDadJoke();
+  displayJoke(joke);
+};
+
+// load 1st joke
+refreshJoke();
+
+setInterval(refreshJoke, 10000);
